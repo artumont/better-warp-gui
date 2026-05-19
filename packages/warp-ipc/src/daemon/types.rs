074194c025a2f1d714c0d7c0d4f50dcc4a004e8c
artumont <197291181+artumont@users.noreply.github.com>
2026-05-19 13:37:00 -0600

    docs(types): correct comment formatting for OperationMode enum

--- packages/warp-ipc/src/daemon/types.rs
+++ packages/warp-ipc/src/daemon/types.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-// Determines the type of connection used by the Warp service, such as "Warp" for a standard VPN connection, "DnsOverTls" for DNS over TLS, "DnsOverHttps" for DNS over HTTPS, and "WarpWithDnsOverHttps" for a combination of both Warp and DNS over HTTPS. This is used in the `set_mode` operation to specify the desired connection type.
+/// Determines the type of connection used by the Warp service, such as "Warp" for a standard VPN connection, "DnsOverTls" for DNS over TLS, "DnsOverHttps" for DNS over HTTPS, and "WarpWithDnsOverHttps" for a combination of both Warp and DNS over HTTPS. This is used in the `set_mode` operation to specify the desired connection type.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(untagged, rename_all = "camelCase")]
 pub enum OperationMode {
